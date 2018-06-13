9b65d45372013725654e0e94c9648378e053c47f
masons7 <itsstef@gmail.com>
2018-06-13 14:32:02 -0700

    some basic helper functions set up

--- 01_main.ino
+++ 01_main.ino
@@ -1,11 +1,15 @@
 /* Variables
 /* ---------------------------------------------------------------------------*/
 uint8_t gHue = 0;
-CRGBPalette16 current_horn_palette;
+uint8_t start_index = 0;
+uint8_t step_index = 4;
+
+CRGBPalette16 current_antler_palette;
 CRGBPalette16 current_face_palette;
 CRGBPalette16 current_wing_palette;
 CRGBPalette16 current_board_palette;
-CRGBPalette16 target_horn_palette;
+
+CRGBPalette16 target_antler_palette;
 CRGBPalette16 target_face_palette;
 CRGBPalette16 target_wing_palette;
 CRGBPalette16 target_board_palette;
@@ -16,11 +20,12 @@ void loop()
   /* -------------------------------------------------------------------------*/
   EVERY_N_MILLISECONDS( 20 ) { gHue++; }
   //EVERY_N_SECONDS( 10 ) { nextPattern(); }
+  start_index++;
 
 
   /* Strand tests
   /* -------------------------------------------------------------------------*/
-  fill_solid(horn_leds, NUM_FACE_LEDS, CRGB::Red);
+  fill_solid(antler_leds, NUM_ANTLER_LEDS, CRGB::Red);
   fill_solid(face_leds, NUM_FACE_LEDS, CRGB::Green);
   fill_solid(wing_leds, NUM_WING_LEDS, CRGB::Blue);
   fill_rainbow(board_leds, NUM_BOARD_LEDS, gHue, 7);
@@ -36,19 +41,20 @@ void loop()
   //   - the default of 24 is a good balance
   //   - meaningful values are 1-48.  1=veeeeeeeery slow, 48=quickest
   //   - "0" means do not change the currentPalette at all; freeze
-  uint8_t maxChanges = 24;
-  nblendPaletteTowardPalette( current_horn_palette, target_horn_palette, maxChanges);
-
-  /* -------------------------------------------------------------------------*/
+  uint8_t max_changes = 24;
+  nblendPaletteTowardPalette( current_antler_palette, target_antler_palette, max_changes);
 
 
 
   /* Send the data to the strips
   /* -------------------------------------------------------------------------*/
+  fill_antlers();
+  fill_face();
+  fill_wings();
+  fill_board();
   FastLED.show();
   FastLED.delay(1000/ANIMATION_SPEED);
 }
-
 
 // Note to self: Can set brightness of individual strips:
 // https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples
--- furfur.ino
+++ furfur.ino
@@ -15,12 +15,12 @@
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
 
-#define HORN_DATA_PIN 6
+#define ANTLER_DATA_PIN 6
 #define FACE_DATA_PIN 9
 #define WING_DATA_PIN 10
 #define BOARD_DATA_PIN 17
 
-#define NUM_HORN_LEDS 64
+#define NUM_ANTLER_LEDS 64
 #define NUM_FACE_LEDS 33
 #define NUM_WING_LEDS 24
 #define NUM_BOARD_LEDS 10
@@ -28,9 +28,13 @@
 #define BRIGHTNESS 255
 #define ANIMATION_SPEED 120
 
+/* Palette Declarations
+/* ---------------------------------------------------------------------------*/
+
+
 /* Setup
 /* ---------------------------------------------------------------------------*/
-CRGB horn_leds[NUM_HORN_LEDS];
+CRGB antler_leds[NUM_ANTLER_LEDS];
 CRGB face_leds[NUM_FACE_LEDS];
 CRGB wing_leds[NUM_WING_LEDS];
 CRGB board_leds[NUM_BOARD_LEDS];
@@ -38,7 +42,7 @@ CRGB board_leds[NUM_BOARD_LEDS];
 void setup() {
   delay(3000);
 
-  FastLED.addLeds<LED_TYPE,HORN_DATA_PIN,COLOR_ORDER>(horn_leds, NUM_HORN_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE,ANTLER_DATA_PIN,COLOR_ORDER>(antler_leds, NUM_ANTLER_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.addLeds<LED_TYPE,FACE_DATA_PIN,COLOR_ORDER>(face_leds, NUM_FACE_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.addLeds<LED_TYPE,WING_DATA_PIN,COLOR_ORDER>(wing_leds, NUM_WING_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.addLeds<LED_TYPE,BOARD_DATA_PIN,COLOR_ORDER>(board_leds, NUM_BOARD_LEDS).setCorrection(TypicalLEDStrip);
